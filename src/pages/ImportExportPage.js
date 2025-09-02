029a4d7ebd66c5e7af148dc3a5e787cde3717d39
Ammy Meshram <ammymesh.clg@gmail.com>
2025-09-02 07:31:32 +0530

    Create ImportExportPage.js

---
+++ src/pages/ImportExportPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ImportExport from "../components/ImportExport";
+
+function ImportExportPage() {
+  return (
+    <div className="p-6">
+      <ImportExport />
+    </div>
+  );
+}
+
+export default ImportExportPage;
